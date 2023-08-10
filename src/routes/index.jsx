ffb06061cc85110153c0ed847d76d371273754f2
GitLabSJ <aeeou12@gmail.com>
2023-08-11 01:36:07 +0900

    Feat: 비급여 항목 데이터 XML to JSON

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,9 +1,38 @@
-import React, { useContext } from "react";
-import { Context } from "../context/contextProvider";
+import React, { useState, useEffect } from "react";
+import XMLParser from "react-xml-parser";
+
 export default function Index() {
-  const { loggedUser } = useContext(Context);
-  console.log(loggedUser.id, loggedUser.email, loggedUser.token);
-  
-  
-  return <div>home</div>;
+  const [data, setData] = useState([]);
+
+  const SERVICE_KEY = process.env.REACT_APP_HEALTH_PRICE_SERVICE_KEY;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `/B551182/nonPaymentDamtInfoService/getNonPaymentItemClcdList?pageNo=1&numOfRows=2&ServiceKey=${SERVICE_KEY}`
+        );
+        const xmlResponse = await response.text();
+        // XML to JSON
+        const jsonObj = new XMLParser().parseFromString(xmlResponse);
+        const tag = jsonObj.getElementsByTagName("npayKorNm");
+        setData(tag);
+      } catch (error) {
+        console.error("API 요청 중 에러 발생:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  console.log(data);
+  return (
+    <div>
+      {data && (
+        <ol>
+          {data.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ol>
+      )}
+    </div>
+  );
 }
